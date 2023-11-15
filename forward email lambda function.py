0ef9c80922b444baff1b3ba48511f13d5d9b0915
Dante <dante@Dantes-Air.fritz.box>
2023-11-15 15:38:01 +0100

    receving emails improved to receive whole body text

--- forward email lambda function.py
+++ forward email lambda function.py
@@ -1,15 +1,3 @@
-# Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# This file is licensed under the Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License. A copy of the
-# License is located at
-#
-# http://aws.amazon.com/apache2.0/
-#
-# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
-# OF ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limitations under the License.
-
 import os
 import boto3
 import email
@@ -21,33 +9,33 @@ from email.mime.application import MIMEApplication
 
 region = os.environ['Region']
 
-# def get_message_from_s3(message_id):
+def get_message_from_s3(message_id):
 
-#     incoming_email_bucket = os.environ['MailS3Bucket']
-#     incoming_email_prefix = os.environ['MailS3Prefix']
+    incoming_email_bucket = os.environ['MailS3Bucket']
+    incoming_email_prefix = os.environ['MailS3Prefix']
 
-#     if incoming_email_prefix:
-#         object_path = (incoming_email_prefix + "/" + message_id)
-#     else:
-#         object_path = message_id
+    if incoming_email_prefix:
+        object_path = (incoming_email_prefix + "/" + message_id)
+    else:
+        object_path = message_id
 
-#     object_http_path = (f"http://s3.console.aws.amazon.com/s3/object/{incoming_email_bucket}/{object_path}?region={region}")
+    object_http_path = (f"http://s3.console.aws.amazon.com/s3/object/{incoming_email_bucket}/{object_path}?region={region}")
 
-#     # Create a new S3 client.
-#     client_s3 = boto3.client("s3")
+    # Create a new S3 client.
+    client_s3 = boto3.client("s3")
 
-#     # Get the email object from the S3 bucket.
-#     object_s3 = client_s3.get_object(Bucket=incoming_email_bucket,
-#         Key=object_path)
-#     # Read the content of the message.
-#     file = object_s3['Body'].read()
+    # Get the email object from the S3 bucket.
+    object_s3 = client_s3.get_object(Bucket=incoming_email_bucket,
+        Key=object_path)
+    # Read the content of the message.
+    file = object_s3['Body'].read()
 
-#     file_dict = {
-#         "file": file,
-#         "path": object_http_path
-#     }
-#     print(file_dict)
-#     return file_dict
+    file_dict = {
+        "file": file,
+        "path": object_http_path
+    }
+    # print(file_dict)
+    return file_dict
 
 def create_message(file_dict):
 
@@ -62,11 +50,17 @@ def create_message(file_dict):
     # Create a new subject line.
     subject_original = mailobject['Subject']
     subject = "FW: " + subject_original
+    
+    # Extract the HTML part of the email body
+    html_part = None
+    for part in mailobject.walk():
+        if part.get_content_type() == 'text/html':
+            html_part = part.get_payload(decode=True).decode('utf-8')
+            break
 
     # The body text of the email.
-    body_text = ("The attached message was received from "
-              + separator.join(mailobject.get_all('From'))
-              + ". This message is archived at " + file_dict['path'])
+    body_text = html_part if html_part else mailobject.get_payload()
+
 
     # The file name to use for the attached message. Uses regex to remove all
     # non-alphanumeric characters, and appends a file extension.
@@ -100,45 +94,45 @@ def create_message(file_dict):
     return message
 
 
-# def send_email(message):
-#     aws_region = os.environ['Region']
+def send_email(message):
+    aws_region = os.environ['Region']
 
-# # Create a new SES client.
-#     client_ses = boto3.client('ses', region)
+# Create a new SES client.
+    client_ses = boto3.client('ses', region)
 
-#     # Send the email.
-#     try:
-#         #Provide the contents of the email.
-#         response = client_ses.send_raw_email(
-#             Source=message['Source'],
-#             Destinations=[
-#                 message['Destinations']
-#             ],
-#             RawMessage={
-#                 'Data':message['Data']
-#             }
-#         )
+    # Send the email.
+    try:
+        #Provide the contents of the email.
+        response = client_ses.send_raw_email(
+            Source=message['Source'],
+            Destinations=[
+                message['Destinations']
+            ],
+            RawMessage={
+                'Data':message['Data']
+            }
+        )
 
-#     # Display an error if something goes wrong.
-#     except ClientError as e:
-#         output = e.response['Error']['Message']
-#     else:
-#         output = "Email sent! Message ID: " + response['MessageId']
+    # Display an error if something goes wrong.
+    except ClientError as e:
+        output = e.response['Error']['Message']
+    else:
+        output = "Email sent! Message ID: " + response['MessageId']
 
-#     return output
+    return output
 
-# def lambda_handler(event, context):
-#     # Get the unique ID of the message. This corresponds to the name of the file
-#     # in S3.
-#     message_id = event['Records'][0]['ses']['mail']['messageId']
-#     print(f"Received message ID {message_id}")
+def lambda_handler(event, context):
+    # Get the unique ID of the message. This corresponds to the name of the file
+    # in S3.
+    message_id = event['Records'][0]['ses']['mail']['messageId']
+    print(f"Received message ID {message_id}")
 
-#     # Retrieve the file from the S3 bucket.
-#     file_dict = get_message_from_s3(message_id)
+    # Retrieve the file from the S3 bucket.
+    file_dict = get_message_from_s3(message_id)
 
-#     # Create the message.
-#     message = create_message(file_dict)
+    # Create the message.
+    message = create_message(file_dict)
 
-#     # Send the email and print the result.
-#     result = send_email(message)
-#     print(result)
+    # Send the email and print the result.
+    result = send_email(message)
+    print(result)
